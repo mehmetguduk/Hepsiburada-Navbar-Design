d6d065af01eee69ccaed98c364f10b4a4d28e05a
mehmetguduk <mehmetguduk98@gmail.com>
2022-03-01 02:33:31 +0300

    main

--- script.js
+++ script.js
@@ -14,9 +14,9 @@ A_ELEMENTS.forEach(a => {
 
 /*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@_HEPSIBURADA_@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*/
 
-const ID_HEPSIBURADA_2 = document.querySelector("#hepsiburada-2");
-const ID_HEPSIBURADA_3 = document.querySelector("#hepsiburada-3");
-const CLASS_SEARCH_INPUT = document.querySelector(".search-input");
+const ID_HEPSIBURADA_2 = document.querySelector("h1#hepsiburada-2");
+const ID_HEPSIBURADA_3 = document.querySelector("h1#hepsiburada-3");
+const CLASS_SEARCH_INPUT = document.querySelector("input.search-input");
 
 CLASS_SEARCH_INPUT.addEventListener("focus", function () {
     ID_HEPSIBURADA_2.style.display = "none";
@@ -30,8 +30,7 @@ CLASS_SEARCH_INPUT.addEventListener("blur", function () {
 
 /*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@_USER_DROPDOWN_@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*/
 
-const CLASS_USER = document.querySelector(".user");
-const CLASS_USER_NAME = document.querySelector("a.user-name");
+const CLASS_USER = document.querySelector("div.user");
 const CLASS_USER_ICON = document.querySelector("i.user-icon");
 const CLASS_DROPDOWN_ITEM = document.querySelectorAll("a.dropdown-item");
 
@@ -40,13 +39,27 @@ function USER_TOGGLE() {
     CLASS_USER.setAttribute("is_active", !(is_active));
 }
 
-function FOCUS_OUT() {
-    // This function works with "onmouseleave" attribute of "div.user" element.
+function USER_FOCUS_OUT() {
     CLASS_USER.setAttribute("is_active", "false");
 }
 
 CLASS_USER_ICON.addEventListener("click", USER_TOGGLE);
-CLASS_USER_NAME.addEventListener("click", USER_TOGGLE);
 CLASS_DROPDOWN_ITEM.forEach(element => {
     element.addEventListener("click", USER_TOGGLE);
 })
+
+/*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@_BASKET_DROPDOWN_@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*/
+
+const CLASS_BASKET = document.querySelector("div.basket");
+const CLASS_BASKET_ICON = document.querySelector("i.basket-icon");
+
+function BASKET_TOGGLE() {
+    let is_active = (CLASS_BASKET.getAttribute("is_active") === "true");
+    CLASS_BASKET.setAttribute("is_active", !(is_active));
+}
+
+function BASKET_FOCUS_OUT() {
+    CLASS_BASKET.setAttribute("is_active", "false");
+}
+
+CLASS_BASKET_ICON.addEventListener("click", BASKET_TOGGLE);
